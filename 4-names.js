1fa7d0dd0ada8e0c146865336f4cd047d61cd51f
CultistaCoC <fimv705@gmail.com>
2023-05-13 23:19:48 -0600

    Added 2 new ways to export stuff

--- 4-names.js
+++ 4-names.js
@@ -4,4 +4,4 @@ const secret = 'SUPER SECRET'
 const john = 'John'
 const peter = 'Peter'
 
-module.exports = { john, peter}
+module.exports = { john, peter }
--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 // Modules - Encapsulated Code (only share minimum)
 const names = require('./4-names')
 const sayHi = require('./5-utils')
+const data = require('./6-alternative-flavor')
+require('./7-mind.grenade')
 
 sayHi("Susan")
 sayHi(names.john)
